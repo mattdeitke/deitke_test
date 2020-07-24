def720ed54e1fc4f99d6ccfea14242f173ec5779
Matt Deitke <mdeitke23@gmail.com>
2020-07-24 14:47:49 -0700

    initial

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='deitke_test',
+      version='0.1.0',
+      author='Matt Deitke',
+      author_email='mdeitke23@gmail.com',
+      license='MIT',
+      packages=['deitke_test'],
+      zip_safe=False)
